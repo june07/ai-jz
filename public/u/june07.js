4e0c9c2dcb396c34e10cd328eb5dd2158e9e578c
BraNFT™ <99556664+branfts@users.noreply.github.com>
2024-07-23 10:36:35 -0700

    Update june07.js

--- public/u/june07.js
+++ public/u/june07.js
@@ -1,17 +1,17 @@
 const links = [
     {
-        href: 'https://june07.com',
+        url: 'https://june07.com',
         title: 'June07 Homepage',
         favicon: 'https://june07.com/favicon.ico'
     },
     {
-        href: 'https://june07.com/searchable-node-js-docs-plus-more',
+        url: 'https://june07.com/searchable-node-js-docs-plus-more',
         title: 'NiM Update',
         subtitle: 'Expanded Tools and Searchable Node.js Docs',
         favicon: 'https://june07.com/favicon.ico'
     },
     {
-        href: 'https://nim.june07.com',
+        url: 'https://nim.june07.com',
         title: 'NiM Homepage',
         favicon: 'https://nim.june07.com/favicon.ico'
     },
@@ -20,17 +20,17 @@ const rules = [
     {
         priority: 0,
         name: 'redirect',
-        href: 'https://june07.com',
+        url: 'https://june07.com',
         timeout: 10
     },{
         priority: 0,
         name: 'redirect',
-        href: 'https://june07.com/searchable-node-js-docs-plus-more',
+        url: 'https://june07.com/searchable-node-js-docs-plus-more',
         timeout: 10
     },{
         priority: 0,
         name: 'redirect',
-        href: 'https://nim.june07.com',
+        url: 'https://nim.june07.com',
         timeout: 10
     }
 ]
