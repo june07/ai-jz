7880515e1bb0c5802dc9f383d5b1c24ab1fb1850
branfts <support@june07.com>
2024-07-22 21:49:22 -0700

    Updated by A10x Service

---
+++ public/u/jolenemendez.js
@@ -0,0 +1,29 @@
+
+const links = [
+    {
+        "url": "https://goto.target.com/c/3288164/1799385/21323?u=https://www.target.com/creators/jolenemendez"
+    },
+    {
+        "url": "https://www.shopltk.com/explore/JoMama"
+    },
+    {
+        "url": "https://www.amazon.com/hz/wishlist/ls/1H857SRN9H90W/ref=nav_wishlist_lists_2?_encoding=UTF8&type=wishlist"
+    },
+    {
+        "url": "https://cash.app/$JoleneMendez"
+    },
+    {
+        "url": "https://www.instagram.com/jolene.mendez"
+    },
+    {
+        "url": "https://www.facebook.com/jomamatok/"
+    },
+    {
+        "url": "https://direct.me/jolenemendez"
+    },
+    {
+        "url": "https://tiktok.com/@jolenemendez"
+    }
+];
+const rules = [];
+export { links, rules };
