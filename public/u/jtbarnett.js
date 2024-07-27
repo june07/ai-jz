b33c0064bb2da1592e947e520a231c7560ddca58
branfts <support@june07.com>
2024-07-27 13:54:57 -0700

    Updated by A10x Service

---
+++ public/u/jtbarnett.js
@@ -0,0 +1,29 @@
+
+const links = [
+    {
+        "url": "https://www.eliteprospects.com/player/37268/j.t.-barnett"
+    },
+    {
+        "url": "https://intro.co/JTBarnett?source=intro"
+    },
+    {
+        "url": "https://twitter.com/jtbarnett?lang=en"
+    },
+    {
+        "url": "https://stan.store/jtbarnett"
+    },
+    {
+        "url": "https://www.instagram.com/jtbarnett"
+    },
+    {
+        "url": "https://linkedin.com/in/jtbarnett"
+    },
+    {
+        "url": "https://www.youtube.com/@jtbarnett."
+    },
+    {
+        "url": "https://www.tiktok.com/@jtbarnett"
+    }
+];
+const rules = [];
+export { links, rules };
