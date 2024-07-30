7c87340d7955f41e9457585e2eaab6fd6f275588
branfts <support@june07.com>
2024-07-29 17:44:36 -0700

    Updated by A10x Service

---
+++ public/u/juliagmonty.js
@@ -0,0 +1,13 @@
+
+const links = [
+    { url: 'https://www.instagram.com/runninglateinnyc/?hl=en' },
+{ url: 'https://www.threads.net/@juliagmonty?xmt=AQGzxW_lmkegKFdC4GhXeySlzVuw249pvB0R3ek36aqjQ1Y' },
+{ url: 'https://www.instagram.com/juliagmonty/?hl=en' },
+{ url: 'mailto:jules@select.co' },
+{ url: 'https://www.tiktok.com/@juliagmonty' }
+]
+const rules = [
+    
+]
+
+export { links, rules }
